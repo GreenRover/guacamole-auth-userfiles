d29b4cc16fb434358af6fad4ae77cc8e6e5bd997
Heiko Henning <henning@mst.ch>
2015-10-07 10:34:16 +0200

    Reload config xml on each each page refresh.

--- src/main/java/net/sourceforge/guacamole/net/auth/userfiles/UserFilesAuthenticationProvider.java
+++ src/main/java/net/sourceforge/guacamole/net/auth/userfiles/UserFilesAuthenticationProvider.java
@@ -7,12 +7,11 @@ import java.io.FileReader;
 import java.io.IOException;
 import java.io.Reader;
 import java.util.Date;
-import java.util.regex.Pattern;
+import java.util.UUID;
 import org.glyptodon.guacamole.GuacamoleException;
 import org.glyptodon.guacamole.GuacamoleServerException;
 import org.glyptodon.guacamole.environment.Environment;
 import org.glyptodon.guacamole.environment.LocalEnvironment;
-import org.glyptodon.guacamole.net.auth.simple.SimpleAuthenticationProvider;
 import org.glyptodon.guacamole.net.auth.Credentials;
 import org.glyptodon.guacamole.protocol.GuacamoleConfiguration;
 import org.slf4j.LoggerFactory;
@@ -22,6 +21,13 @@ import org.xml.sax.SAXException;
 import org.xml.sax.XMLReader;
 import org.xml.sax.helpers.XMLReaderFactory;
 import javax.servlet.http.HttpServletRequest;
+import org.glyptodon.guacamole.net.auth.AbstractAuthenticatedUser;
+import org.glyptodon.guacamole.net.auth.AuthenticatedUser;
+import org.glyptodon.guacamole.net.auth.AuthenticationProvider;
+import org.glyptodon.guacamole.net.auth.UserContext;
+import org.glyptodon.guacamole.net.auth.simple.SimpleUserContext;
+import org.glyptodon.guacamole.token.StandardTokens;
+import org.glyptodon.guacamole.token.TokenFilter;
 
 /**
  * Disable authentication in Guacamole. All users accessing Guacamole are
@@ -51,23 +57,18 @@ import javax.servlet.http.HttpServletRequest;
  * 
  * @author Heiko Henning
  */
-public class UserFilesAuthenticationProvider extends SimpleAuthenticationProvider {
+public class UserFilesAuthenticationProvider implements AuthenticationProvider {
 
     /**
      * Logger for this class.
      */
-    private Logger logger = LoggerFactory.getLogger(UserFilesAuthenticationProvider.class);
-
-    /**
-     * Map of all known configurations, indexed by identifier.
-     */
-    private Map<String, GuacamoleConfiguration> configs;
+    private final Logger logger = LoggerFactory.getLogger(UserFilesAuthenticationProvider.class);
 
     /**
      * The last time the configuration XML was modified, as milliseconds since
      * UNIX epoch.
      */
-    private long configTime;
+    //private long config_time;
 
     /**
      * Guacamole server environment.
@@ -80,6 +81,84 @@ public class UserFilesAuthenticationProvider extends SimpleAuthenticationProvide
      */
     public static final String DEFAULT_NOAUTH_CONFIG = "noauth-config.xml";
 
+    /**
+     * AuthenticatedUser which contains its own predefined set of authorized
+     * configurations.
+     *
+     * @author Michael Jumper
+     */
+    private class UserFilesAuthenticatedUser extends AbstractAuthenticatedUser {
+
+        /**
+         * The credentials provided when this AuthenticatedUser was
+         * authenticated.
+         */
+        private final Credentials credentials;
+
+        /**
+         * The GuacamoleConfigurations that this AuthenticatedUser is
+         * authorized to use.
+         */
+        private Map<String, GuacamoleConfiguration> configs;
+
+        /**
+         * Creates a new SimpleAuthenticatedUser associated with the given
+         * credentials and having access to the given Map of
+         * GuacamoleConfigurations.
+         *
+         * @param credentials
+         *     The credentials provided by the user when they authenticated.
+         *
+         * @param configs
+         *     A Map of all GuacamoleConfigurations for which this user has
+         *     access. The keys of this Map are Strings which uniquely identify
+         *     each configuration.
+         */
+        public UserFilesAuthenticatedUser(Credentials credentials, Map<String, GuacamoleConfiguration> configs) {
+
+            // Store credentials and configurations
+            this.credentials = credentials;
+            this.configs = configs;
+
+            // Pull username from credentials if it exists
+            String username = credentials.getUsername();
+            if (username != null && !username.isEmpty()) {
+                setIdentifier(username);
+
+            // Otherwise generate a random username
+            } else {
+                setIdentifier(UUID.randomUUID().toString());
+            }
+
+        }
+
+        /**
+         * Returns a Map containing all GuacamoleConfigurations that this user
+         * is authorized to use. The keys of this Map are Strings which
+         * uniquely identify each configuration.
+         *
+         * @return
+         *     A Map of all configurations for which this user is authorized.
+         */
+        public Map<String, GuacamoleConfiguration> getAuthorizedConfigurations() {
+            return configs;
+        }
+        
+        public void setAuthorizedConfigurations(Map<String, GuacamoleConfiguration> configs) {
+            this.configs = configs;
+        }
+
+        @Override
+        public AuthenticationProvider getAuthenticationProvider() {
+            return UserFilesAuthenticationProvider.this;
+        }
+
+        @Override
+        public Credentials getCredentials() {
+            return credentials;
+        }
+    }
+    
     /**
      * Creates a new UserFilesAuthenticationProvider that does not perform any
      * authentication at all. All attempts to access the Guacamole system are
@@ -122,14 +201,47 @@ public class UserFilesAuthenticationProvider extends SimpleAuthenticationProvide
         }
 
         return configFile;
-
     }
     
-    public synchronized void init() throws GuacamoleException {
-        init("");
+    /**
+     * Parse guacamole configuration xml.
+     * 
+     * @param username
+     * @param ident
+     * @return
+     * @throws GuacamoleException 
+     */
+    public Map<String, GuacamoleConfiguration> parseConfigFile(String username, String ident) throws GuacamoleException {
+        String prefix = null;
+        if (!username.isEmpty() && !ident.isEmpty()) {
+            prefix = username + "_" + ident + "_";
+        } else if (!ident.isEmpty()) {
+            prefix = "anonymous_" + ident + "_";
+        }
+        
+        // Check mapping file mod time
+        File configFile = getConfigurationFile(prefix);
+        Map<String, GuacamoleConfiguration> configs = null;
+        
+        if (configFile.exists()) {
+
+            // If modified recently, gain exclusive access and recheck
+            synchronized (this) {
+                if (configFile.exists()) {
+                    logger.debug("Parse configuration file \"{}\".", configFile);
+                    configs = parseConfigFile(prefix); // If still not up to date, re-init
+                }
+            }
+        }
+        
+        return configs;
+    }
+    
+    public synchronized Map<String, GuacamoleConfiguration> parseConfigFile() throws GuacamoleException {
+        return parseConfigFile("");
     }
 
-    public synchronized void init(String prefix) throws GuacamoleException {
+    public synchronized Map<String, GuacamoleConfiguration> parseConfigFile(String prefix) throws GuacamoleException {
 
         // Get configuration file
         File configFile = getConfigurationFile(prefix);
@@ -149,11 +261,13 @@ public class UserFilesAuthenticationProvider extends SimpleAuthenticationProvide
             parser.parse(new InputSource(reader));
             reader.close();
             
+            Map<String, GuacamoleConfiguration> configs = null;
+            
             // Check if config is valid and use/init.
             Date now = new Date();
             if (contentHandler.getValidTo() == null || contentHandler.getValidTo().compareTo(now) >= 0) {
-                // Init configs
-                configTime = configFile.lastModified();
+                // Parse configs xml file to Object.
+                //config_time = configFile.lastModified();
                 configs = contentHandler.getConfigs();
             } else {
                 logger.warn("Ignore config: \"{}\" because its valid_to \"{}\" is outdated.", configFile, contentHandler.getValidTo().toString());
@@ -167,6 +281,8 @@ public class UserFilesAuthenticationProvider extends SimpleAuthenticationProvide
                     logger.warn("Error deleting config file: \"{}\": \"{}\"", configFile, e.getMessage());
                 }
             }
+            
+            return configs;
 
         }
         catch (IOException e) {
@@ -178,13 +294,12 @@ public class UserFilesAuthenticationProvider extends SimpleAuthenticationProvide
 
     }
 
-    @Override
     public Map<String, GuacamoleConfiguration> getAuthorizedConfigurations(Credentials credentials) throws GuacamoleException {
         HttpServletRequest request = credentials.getRequest();
         
         String username = request.getParameter("username");
         String ident = request.getParameter("ident");
-        
+
         // Avoid null point exceptions.
         if (username == null) {
             username = "";
@@ -206,27 +321,7 @@ public class UserFilesAuthenticationProvider extends SimpleAuthenticationProvide
         }
         **/
         
-        String prefix = null;
-        if (!username.isEmpty() && !ident.isEmpty()) {
-            prefix = username + "_" + ident + "_";
-        } else if (!ident.isEmpty()) {
-            prefix = "anonymous_" + ident + "_";
-        }
-        
-        // Check mapping file mod time
-        File configFile = getConfigurationFile(prefix);
-        
-        if (configFile.exists() && configTime < configFile.lastModified()) {
-
-            // If modified recently, gain exclusive access and recheck
-            synchronized (this) {
-                if (configFile.exists() && configTime < configFile.lastModified()) {
-                    logger.debug("Configuration file \"{}\" has been modified.", configFile);
-                    init(prefix); // If still not up to date, re-init
-                }
-            }
-
-        }
+        Map<String, GuacamoleConfiguration> configs = parseConfigFile(username, ident);
 
         // If no mapping available, report as such
         if (configs == null) {
@@ -240,6 +335,141 @@ public class UserFilesAuthenticationProvider extends SimpleAuthenticationProvide
         }
 
         return configs;
+    }
+    
+    /**
+     * Given an arbitrary credentials object, returns a Map containing all
+     * configurations authorized by those credentials, filtering those
+     * configurations using a TokenFilter and the standard credential tokens
+     * (like ${GUAC_USERNAME} and ${GUAC_PASSWORD}). The keys of this Map
+     * are Strings which uniquely identify each configuration.
+     *
+     * @param credentials
+     *     The credentials to use to retrieve authorized configurations.
+     *
+     * @return
+     *     A Map of all configurations authorized by the given credentials, or
+     *     null if the credentials given are not authorized.
+     *
+     * @throws GuacamoleException
+     *     If an error occurs while retrieving configurations.
+     */
+    private Map<String, GuacamoleConfiguration> getFilteredAuthorizedConfigurations(Credentials credentials)
+            throws GuacamoleException {
 
+        // Get configurations
+        Map<String, GuacamoleConfiguration> configs = getAuthorizedConfigurations(credentials);
+
+        // Return as unauthorized if not authorized to retrieve configs
+        if (configs == null) {
+            return null;
+        }
+
+        // Build credential TokenFilter
+        TokenFilter tokenFilter = new TokenFilter();
+        StandardTokens.addStandardTokens(tokenFilter, credentials);
+
+        // Filter each configuration
+        for (GuacamoleConfiguration config : configs.values()) {
+            tokenFilter.filterValues(config.getParameters());
+        }
+        
+        return configs;
+    }
+
+    /**
+     * Given a user who has already been authenticated, returns a Map
+     * containing all configurations for which that user is authorized,
+     * filtering those configurations using a TokenFilter and the standard
+     * credential tokens (like ${GUAC_USERNAME} and ${GUAC_PASSWORD}). The keys
+     * of this Map are Strings which uniquely identify each configuration.
+     *
+     * @param authenticatedUser
+     *     The user whose authorized configurations are to be retrieved.
+     *
+     * @return
+     *     A Map of all configurations authorized for use by the given user, or
+     *     null if the user is not authorized to use any configurations.
+     *
+     * @throws GuacamoleException
+     *     If an error occurs while retrieving configurations.
+     */
+    private Map<String, GuacamoleConfiguration> getFilteredAuthorizedConfigurations(AuthenticatedUser authenticatedUser)
+            throws GuacamoleException {
+
+        // Pull cached configurations, if any
+        if (authenticatedUser instanceof UserFilesAuthenticationProvider.UserFilesAuthenticatedUser && authenticatedUser.getAuthenticationProvider() == this) {
+            return ((UserFilesAuthenticationProvider.UserFilesAuthenticatedUser) authenticatedUser).getAuthorizedConfigurations();
+        }
+        
+        // Otherwise, pull using credentials
+        return getFilteredAuthorizedConfigurations(authenticatedUser.getCredentials());
+
+    }
+
+    @Override
+    public AuthenticatedUser authenticateUser(final Credentials credentials)
+            throws GuacamoleException {
+
+        // Get configurations
+        Map<String, GuacamoleConfiguration> configs = getFilteredAuthorizedConfigurations(credentials);
+
+        // Return as unauthorized if not authorized to retrieve configs
+        if (configs == null) {
+            return null;
+        }
+
+        return new UserFilesAuthenticationProvider.UserFilesAuthenticatedUser(credentials, configs);
+
+    }
+
+    @Override
+    public UserContext getUserContext(AuthenticatedUser authenticatedUser)
+            throws GuacamoleException {
+
+        // Get configurations
+        Map<String, GuacamoleConfiguration> configs = getFilteredAuthorizedConfigurations(authenticatedUser);
+
+        // Return as unauthorized if not authorized to retrieve configs
+        if (configs == null) {
+            return null;
+        }
+
+        // Return user context restricted to authorized configs
+        return new SimpleUserContext(this, authenticatedUser.getIdentifier(), configs);
+
+    }
+
+    @Override
+    // Re parse config xml for loged in user on each page refresh.
+    public AuthenticatedUser updateAuthenticatedUser(AuthenticatedUser authenticatedUser, Credentials credentials) 
+            throws GuacamoleException {
+    
+        // Get configurations
+        Map<String, GuacamoleConfiguration> configs = getFilteredAuthorizedConfigurations(credentials);
+
+        // Return as unauthorized if not authorized to retrieve configs
+        if (configs == null) {
+            return null;
+        }
+
+        return new UserFilesAuthenticationProvider.UserFilesAuthenticatedUser(credentials, configs);
+    }
+
+    @Override
+     // Create new user context wih new config.
+    public UserContext updateUserContext(UserContext context, AuthenticatedUser authenticatedUser) 
+            throws GuacamoleException {
+                        
+        // Get configurations
+        Map<String, GuacamoleConfiguration> configs = getFilteredAuthorizedConfigurations(authenticatedUser);
+        
+        // Return as unauthorized if not authorized to retrieve configs
+        if (configs == null) {
+            return null;
+        }
+
+        // Return user context restricted to authorized configs
+        return new SimpleUserContext(this, authenticatedUser.getIdentifier(), configs);
     }
 }
